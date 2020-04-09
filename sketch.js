7701335bb3ec8c4cff9f1d713e34f4c58d48cc3b
TLP309 <lucckythol@gmail.com>
2020-04-09 18:17:14 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -16,7 +18,7 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(700,height,1200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
